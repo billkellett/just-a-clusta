482252107584ea10aa8ba90f7de4f1ef61287532
billkellett <44028163+billkellett@users.noreply.github.com>
2019-03-26 08:04:58 -0400

    Update restartOpsCenter.sh

--- .startup/restartOpsCenter.sh
+++ .startup/restartOpsCenter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Sometimes opsceneter needs to be bounced for the metrics dashboard to appear
+# Sometimes opscenter needs to be bounced for the metrics dashboard to appear
 # May be due to startup order in startup scripts
 # This is temperary until assethub is fixed
 if [ `hostname` == 'node0' ]
